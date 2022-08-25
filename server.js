749fbffab15c78c55427fd33c0785548bf161cc5
Igor Tiburcio Cavalcanti <igor.tiburcio.cs@gmail.com>
2022-08-24 21:24:23 -0300

    Mudança na arquitetura do servidor!

--- server.js
+++ server.js
@@ -2,11 +2,6 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-mongoose.connect(process.env.CONNECTIONSTRING, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    app.emit('okay');
-  })
-  .catch(e => console.log(e));
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
 const flash = require('connect-flash');
@@ -16,6 +11,12 @@ const helmet = require('helmet');
 const csrf = require('csurf');
 const { checkCsrfError, csrfMiddleware } = require('./src/middlewares/middlewares');
 
+mongoose.connect(process.env.CONNECTIONSTRING, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    app.emit('ready');
+  })
+  .catch(e => console.log(e));
+
 app.use(helmet());
 
 app.use(express.urlencoded({ extended: true }));
@@ -23,7 +24,7 @@ app.use(express.json());
 app.use(express.static(path.resolve(__dirname, 'public')));
 
 const sessionOptions = session({
-  secret: 'akasdfj0út23453456+54qt23qv',
+  secret: 'akasdfj0ut23453456+54qt23qv',
   store: MongoStore.create({ mongoUrl: process.env.CONNECTIONSTRING }),
   resave: false,
   saveUninitialized: false,
@@ -32,6 +33,7 @@ const sessionOptions = session({
     httpOnly: true
   }
 });
+
 app.use(sessionOptions);
 app.use(flash());
 
@@ -39,12 +41,12 @@ app.set('views', path.resolve(__dirname, 'src', 'views'));
 app.set('view engine', 'ejs');
 
 app.use(csrf());
-// Nossos próprios middlewares
+
 app.use(checkCsrfError);
 app.use(csrfMiddleware);
 app.use(routes);
 
-app.on('okay', () => {
+app.on('ready', () => {
   app.listen(3000, () => {
     console.log('Acessar http://localhost:3000');
     console.log('Servidor executando na porta 3000');
